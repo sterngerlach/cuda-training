c1e84b8e2164dc3ab84342703f462d3ec9a1e685
Stern Gerlach <std.experimental.optional@gmail.com>
2019-06-15 15:14:00 +0900

    add matrix-sum-1d-grid-1d-block.cu

---
+++ matrix-sum-1d-grid-1d-block.cu
@@ -0,0 +1,232 @@
+
+/* matrix-sum-1d-grid-1d-block.cu */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <cuda_runtime.h>
+
+#define CHECK_CUDA_CALL(call) \
+    { \
+        const cudaError_t error = call; \
+        \
+        if (error != cudaSuccess) { \
+            fprintf(stderr, "Error (%s:%d), code: %d, reason: %s\n", \
+                    __FILE__, __LINE__, \
+                    error, cudaGetErrorString(error)); \
+                exit(EXIT_FAILURE); \
+        } \
+    }
+
+void printMatrix(float* matC, int row, int col)
+{
+    int i;
+    int j;
+    float* pC = matC;
+
+    printf("Matrix (%d, %d)\n", row, col);
+
+    for (i = 0; i < row; ++i) {
+        for (j = 0; j < col; ++j)
+            printf("%5.2f ", pC[j]);
+
+        printf("\n");
+
+        pC += col;
+    }
+
+    printf("\n");
+
+    return;
+}
+
+void checkResult(float* hostResult, float* devResult, int row, int col)
+{
+    const double epsilon = 1.0E-8;
+    
+    int i;
+    int j;
+    float* pHost = hostResult;
+    float* pDev = devResult;
+
+    for (i = 0; i < row; ++i) {
+        for (j = 0; j < col; ++j) {
+            if (fabsf(pHost[j] - pDev[j]) > epsilon) {
+                fprintf(stderr, "Results do not match!\n");
+                fprintf(stderr,
+                        "Index (%d, %d), Host result: %5.2f, Device result: %5.2f\n",
+                        i, j, pHost[j], pDev[j]);
+                return;
+            }
+        }
+
+        pHost += col;
+        pDev += col;
+    }
+
+    printf("Results match.\n");
+
+    return;
+}
+
+void initializeMatrix(float* matC, int row, int col)
+{
+    int i;
+    int j;
+    float* pC = matC;
+
+    srand((unsigned int)time(NULL));
+
+    for (i = 0; i < row; ++i) {
+        for (j = 0; j < col; ++j)
+            pC[j] = (float)(rand() & 0xFF) / 10.0f;
+
+        pC += col;
+    }
+
+    return;
+}
+
+void sumMatrixOnHost(float* matA, float* matB, float* matC, int row, int col)
+{
+    int i;
+    int j;
+    float* pA = matA;
+    float* pB = matB;
+    float* pC = matC;
+
+    for (i = 0; i < row; ++i) {
+        for (j = 0; j < col; ++j)
+            pC[j] = pA[j] + pB[j];
+
+        pA += col;
+        pB += col;
+        pC += col;
+    }
+
+    return;
+}
+
+__global__ void sumMatrixOnGPU1D(float* matA, float* matB, float* matC,
+                                 int row, int col)
+{
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y;
+    int i;
+    
+    if (x < col) {
+        for (y = 0; y < row; ++y) {
+            i = y * col + x;
+            matC[i] = matA[i] + matB[i];
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int dev;
+    cudaDeviceProp deviceProp;
+
+    int matRow;
+    int matCol;
+    int numOfElements;
+    int numOfBytes;
+
+    int blockCol;
+
+    float* hostMatA;
+    float* hostMatB;
+    float* hostMatC;
+    float* devMatA;
+    float* devMatB;
+    float* devMatC;
+    float* devResult;
+
+    struct timeval startTime;
+    struct timeval endTime;
+
+    /* Setup device */
+    dev = 0;
+    CHECK_CUDA_CALL(cudaGetDeviceProperties(&deviceProp, dev));
+    
+    printf("Using device %d: %s\n", dev, deviceProp.name);
+
+    CHECK_CUDA_CALL(cudaSetDevice(dev));
+
+    /* Set matrix size */
+    matCol = 1 << 14;
+    matRow = 1 << 14;
+    numOfElements = matRow * matCol;
+    numOfBytes = numOfElements * sizeof(float);
+
+    /* Allocate host memory */
+    hostMatA = (float*)calloc(numOfElements, sizeof(float));
+    hostMatB = (float*)calloc(numOfElements, sizeof(float));
+    hostMatC = (float*)calloc(numOfElements, sizeof(float));
+    devResult = (float*)calloc(numOfElements, sizeof(float));
+
+    initializeMatrix(hostMatA, matRow, matCol);
+    initializeMatrix(hostMatB, matRow, matCol);
+
+    /* Allocate device memory */
+    CHECK_CUDA_CALL(cudaMalloc((void**)&devMatA, numOfBytes));
+    CHECK_CUDA_CALL(cudaMalloc((void**)&devMatB, numOfBytes));
+    CHECK_CUDA_CALL(cudaMalloc((void**)&devMatC, numOfBytes));
+
+    /* Transfer matrix data from host */
+    CHECK_CUDA_CALL(cudaMemcpy(devMatA, hostMatA, numOfBytes, cudaMemcpyHostToDevice));
+    CHECK_CUDA_CALL(cudaMemcpy(devMatB, hostMatB, numOfBytes, cudaMemcpyHostToDevice));
+    CHECK_CUDA_CALL(cudaMemcpy(devMatC, devResult, numOfBytes, cudaMemcpyHostToDevice));
+
+    /* Set execution configuration */
+    blockCol = 32;
+    dim3 block(blockCol, 1);
+    dim3 grid((matCol + block.x - 1) / block.x, 1);
+    
+    /* Call CUDA kernel from host */
+    gettimeofday(&startTime, NULL);
+    sumMatrixOnGPU1D<<<grid, block>>>(devMatA, devMatB, devMatC, matRow, matCol);
+    CHECK_CUDA_CALL(cudaDeviceSynchronize());
+    gettimeofday(&endTime, NULL);
+
+    printf("Execution configuration: <<<(%d, %d), (%d, %d)>>>\n",
+           grid.x, grid.y, block.x, block.y);
+    printf("Device execution time: %.6f\n",
+           ((double)endTime.tv_sec + (double)endTime.tv_usec * 1.0e-6) -
+           ((double)startTime.tv_sec + (double)startTime.tv_usec * 1.0e-6));
+
+    /* Check kernel error */
+    CHECK_CUDA_CALL(cudaGetLastError());
+
+    /* Copy CUDA kernel result to host */
+    CHECK_CUDA_CALL(cudaMemcpy(devResult, devMatC, numOfBytes, cudaMemcpyDeviceToHost));
+
+    /* Add matrices in host to check result */
+    gettimeofday(&startTime, NULL);
+    sumMatrixOnHost(hostMatA, hostMatB, hostMatC, matRow, matCol);
+    gettimeofday(&endTime, NULL);
+
+    printf("Host execution time: %.6f\n",
+           ((double)endTime.tv_sec + (double)endTime.tv_usec * 1.0e-6) -
+           ((double)startTime.tv_sec + (double)startTime.tv_usec * 1.0e-6));
+
+    /* Check device result */
+    checkResult(hostMatC, devResult, matRow, matCol);
+
+    /* Free device and host memory */
+    CHECK_CUDA_CALL(cudaFree(devMatA));
+    CHECK_CUDA_CALL(cudaFree(devMatB));
+    CHECK_CUDA_CALL(cudaFree(devMatC));
+
+    free(hostMatA);
+    free(hostMatB);
+    free(hostMatC);
+    free(devResult);
+
+    /* Reset device */
+    CHECK_CUDA_CALL(cudaDeviceReset());
+
+    return EXIT_SUCCESS;
+}
+
